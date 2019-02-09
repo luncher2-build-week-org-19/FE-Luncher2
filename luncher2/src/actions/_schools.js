c0665ae06a1f9ad2251287245bf80d505c8f1479
Mike Landers <landers.mike@gmail.com>
2019-02-09 14:26:16 -0800

    completed writing schools actions and reducers

--- luncher2/src/actions/_schools.js
+++ luncher2/src/actions/_schools.js
@@ -12,6 +12,22 @@ export const GET_SCHOOL_BY_ID_START = 'GET_SCHOOL_BY_ID_START';
 export const GET_SCHOOL_BY_ID_SUCCESS = 'GET_SCHOOL_BY_ID_SUCCESS';
 export const GET_SCHOOL_BY_ID_FAILURE = 'GET_SCHOOL_BY_ID_FAILURE';
 
+export const GET_SCHOOLS_BY_USER_ID_START = 'GET_SCHOOLS_BY_USER_ID_START';
+export const GET_SCHOOLS_BY_USER_ID_SUCCESS = 'GET_SCHOOLS_BY_USER_ID_SUCCESS';
+export const GET_SCHOOLS_BY_USER_ID_FAILURE = 'GET_SCHOOLS_BY_USER_ID_FAILURE';
+
+export const ADD_SCHOOL_START = 'ADD_SCHOOL_START';
+export const ADD_SCHOOL_SUCCESS = 'ADD_SCHOOL_SUCCESS';
+export const ADD_SCHOOL_FAILURE = 'ADD_SCHOOL_FAILURE';
+
+export const UPDATE_SCHOOL_START = 'UPDATE_SCHOOL_START';
+export const UPDATE_SCHOOL_SUCCESS = 'UPDATE_SCHOOL_SUCCESS';
+export const UPDATE_SCHOOL_FAILURE = 'UPDATE_SCHOOL_FAILURE';
+
+export const DELETE_SCHOOL_START = 'DELETE_SCHOOL_START';
+export const DELETE_SCHOOL_SUCCESS = 'DELETE_SCHOOL_SUCCESS';
+export const DELETE_SCHOOL_FAILURE = 'DELETE_SCHOOL_FAILURE';
+
 //**********************// GET ALL SCHOOLS
 
 export const getAllSchools = () => dispatch => {
@@ -28,14 +44,87 @@ export const getAllSchools = () => dispatch => {
 
 //**********************// GET SCHOOL BY ID
 
-export const getSchoolById = id => dispatch => {
+export const getSchoolById = schoolID => dispatch => {
 	dispatch({ type: GET_SCHOOL_BY_ID_START });
 	axios({
 		method: 'get',
-		url: `https://luncher-2-bw-19-lambda.herokuapp.com/schools/${id}`,
+		url: `https://luncher-2-bw-19-lambda.herokuapp.com/schools/${schoolID}`,
 	})
 		.then(res => {
 			dispatch({ type: GET_SCHOOL_BY_ID_SUCCESS, payload: res[0] });
 		})
 		.catch(err => dispatch({ type: GET_SCHOOL_BY_ID_FAILURE, payload: err }));
+};
+
+//**********************// GET SCHOOLS BY USER ID
+
+export const getSchoolByUserId = userId => dispatch => {
+	dispatch({ type: GET_SCHOOLS_BY_USER_ID_START });
+	axios({
+		method: 'get',
+		url: `https://luncher-2-bw-19-lambda.herokuapp.com/schools/user/${userId}`,
+	})
+		.then(res => {
+			dispatch({ type: GET_SCHOOLS_BY_USER_ID_SUCCESS, payload: res });
+		})
+		.catch(err => dispatch({ type: GET_SCHOOLS_BY_USER_ID_FAILURE, payload: err }));
+};
+
+//**********************// ADD SCHOOL
+
+export const addSchool = (userToken, school) => dispatch => {
+	dispatch({ type: ADD_SCHOOL_START });
+	axios({
+		method: 'post',
+		url: `https://luncher-2-bw-19-lambda.herokuapp.com/schools/`,
+		headers: {
+			Authorization: userToken,
+		},
+		data: {
+			image: school.image,
+			schoolname: school.schoolname,
+		},
+	})
+		.then(res => {
+			dispatch({ type: ADD_SCHOOL_SUCCESS, payload: res.data });
+		})
+		.catch(err => dispatch({ type: ADD_SCHOOL_FAILURE, payload: err }));
+};
+
+//**********************// UPDATE SCHOOL
+
+export const updateSchool = (userToken, school, schoolID) => dispatch => {
+	dispatch({ type: UPDATE_SCHOOL_START });
+	axios({
+		method: 'put',
+		url: `https://luncher-2-bw-19-lambda.herokuapp.com/schools/update/${schoolID}`,
+		headers: {
+			Authorization: userToken,
+		},
+		data: {
+			schoolname: school.schoolName,
+			image: school.image,
+		},
+	})
+		.then(res => {
+			dispatch({ type: UPDATE_SCHOOL_SUCCESS, payload: res });
+		})
+		.catch(err => dispatch({ type: UPDATE_SCHOOL_FAILURE, payload: err }));
+};
+
+//**********************// DELETE SCHOOL
+
+export const deleteSchool = (userToken, schoolID) => dispatch => {
+	dispatch({ type: DELETE_SCHOOL_START });
+	axios({
+		method: 'delete',
+		url: `https://luncher-2-bw-19-lambda.herokuapp.com/schools/delete/${schoolID}`,
+		headers: {
+			Authorization: userToken,
+		},
+	})
+		.then(res => {
+			dispatch({ type: DELETE_SCHOOL_SUCCESS, payload: res });
+		})
+		.catch(err => dispatch({ type: DELETE_SCHOOL_FAILURE, payload: err }));
 };
--- luncher2/src/reducers/_schools.js
+++ luncher2/src/reducers/_schools.js
@@ -5,18 +5,39 @@ import {
 	GET_SCHOOL_BY_ID_START,
 	GET_SCHOOL_BY_ID_SUCCESS,
 	GET_SCHOOL_BY_ID_FAILURE,
+	GET_SCHOOLS_BY_USER_ID_START,
+	GET_SCHOOLS_BY_USER_ID_SUCCESS,
+	GET_SCHOOLS_BY_USER_ID_FAILURE,
+	ADD_SCHOOL_START,
+	ADD_SCHOOL_SUCCESS,
+	ADD_SCHOOL_FAILURE,
+	UPDATE_SCHOOL_START,
+	UPDATE_SCHOOL_SUCCESS,
+	UPDATE_SCHOOL_FAILURE,
+	DELETE_SCHOOL_START,
+	DELETE_SCHOOL_SUCCESS,
+	DELETE_SCHOOL_FAILURE,
 } from '../actions/_schools';
 
 const initialState = {
 	allSchools: [],
-	schoolById: [],
+	schoolById: '',
+	schoolsByUserId: [],
 	//isLoading
 	isLoading_GetAllSchools: false,
 	isLoading_GetSchoolById: false,
+	isLoading_GetSchoolsByUserId: false,
+	isLoading_AddSchool: false,
+	isLoading_UpdateSchool: false,
+	isLoading_DeleteSchool: false,
 
 	//errors
 	error_GetAllSchools: '',
 	error_GetSchoolById: '',
+	error_GetSschoolsByUserId: '',
+	error_AddSchool: '',
+	error_UpdateSchool: '',
+	error_DeleteSchool: '',
 };
 
 const reducer = (state = initialState, action) => {
@@ -64,6 +85,87 @@ const reducer = (state = initialState, action) => {
 				schoolById: '',
 				isLoading_GetSchoolById: false,
 				error_GetSchoolById: action.payload,
+			};
+		//**********************// GET SCHOOLS BY USER ID
+		case GET_SCHOOLS_BY_USER_ID_START:
+			return {
+				...state,
+				schoolsByUserId: '',
+				isLoading_GetSchoolsByUserId: false,
+				error_GetSschoolsByUserId: '',
+			};
+		case GET_SCHOOLS_BY_USER_ID_SUCCESS:
+			return {
+				...state,
+				schoolsByUserId: action.payload,
+				isLoading_GetSchoolsByUserId: true,
+				error_GetSschoolsByUserId: '',
+			};
+		case GET_SCHOOLS_BY_USER_ID_FAILURE:
+			return {
+				...state,
+				schoolsByUserId: '',
+				isLoading_GetSchoolsByUserId: false,
+				error_GetSschoolsByUserId: action.payload,
+			};
+		//**********************// ADD SCHOOL
+		case ADD_SCHOOL_START:
+			return {
+				...state,
+				isLoading_AddSchool: false,
+				error_AddSchool: '',
+			};
+		case ADD_SCHOOL_SUCCESS:
+			return {
+				...state,
+				isLoading_AddSchool: true,
+				error_AddSchool: '',
+			};
+		case ADD_SCHOOL_FAILURE:
+			return {
+				...state,
+				isLoading_AddSchool: false,
+				error_AddSchool: action.payload,
+			};
+
+		//**********************// GET SCHOOLS BY USER ID
+		case UPDATE_SCHOOL_START:
+			return {
+				...state,
+				isLoading_UpdateSchool: false,
+				error_UpdateSchool: '',
+			};
+		case UPDATE_SCHOOL_SUCCESS:
+			return {
+				...state,
+				isLoading_UpdateSchool: true,
+				error_UpdateSchool: '',
+			};
+		case UPDATE_SCHOOL_FAILURE:
+			return {
+				...state,
+				isLoading_UpdateSchool: false,
+				error_UpdateSchool: action.payload,
+			};
+
+		//**********************// GET SCHOOLS BY USER ID
+		case DELETE_SCHOOL_START:
+			return {
+				...state,
+				isLoading_DeleteSchool: false,
+				error_DeleteSchool: [],
+			};
+		case DELETE_SCHOOL_SUCCESS:
+			return {
+				...state,
+				isLoading_DeleteSchool: true,
+				error_DeleteSchool: '',
+			};
+		case DELETE_SCHOOL_FAILURE:
+			return {
+				...state,
+				isLoading_DeleteSchool: false,
+				error_DeleteSchool: action.payload,
 			};
 
 		default:

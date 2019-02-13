61005412861b863fc8554f5f91b63c4c8e1a0562
Mike Landers <landers.mike@gmail.com>
2019-02-12 16:38:25 -0800

    starting donations received reducer and added in to reducer index

--- luncher2/src/reducers/_index.js
+++ luncher2/src/reducers/_index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import _users from './_users';
 import _schools from './schools';
+import _donation_needs from './_donation_needs';
+import _donations_received from './_donations_received';
 
 export default combineReducers({
 	_users,
 	_schools,
+	_donation_needs,
+	_donations_received,
 });
